3b2919a0906a1166e5a1b77f9840693afb1b82ce
Alejandro Roig <a.roigaguilar@edu.gva.es>
2026-02-19 20:59:58 +0100

    disabled springboottest

--- src/test/java/org/accesodatos/spring/ProyectoSpringBanco2026SpringApplicationTests.java
+++ src/test/java/org/accesodatos/spring/ProyectoSpringBanco2026SpringApplicationTests.java
@@ -1,8 +1,10 @@
 package org.accesodatos.spring;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Deshabilitado en CI/CD porque requiere PostgreSQL real")
 @SpringBootTest
 class ProyectoSpringBanco2026SpringApplicationTests {
 
